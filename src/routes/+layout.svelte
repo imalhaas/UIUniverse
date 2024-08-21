22481d43b09c262bf0bb51cb1634e8a3fd6f3321
Lucas Magalhaes <lucasdecassia9@hotmail.com>
2024-08-21 18:48:48 -0300

    init project

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script>import "../app.css";</script>
+<h1>Layout</h1>
+
+<ul>
+    <li><a href="/">Início</a></li>
+    <li><a href="/sobre">Sobre</a></li>
+    <li><a href="/portfolio">Portfolio</a></li>
+    <li><a href="/contato">Contato</a></li>
+    <li><a href="/faq">Perguntas Frequentes</a></li>
+</ul>
+
+
+
+<slot></slot>
