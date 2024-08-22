72e7cc843f49e9d8e38357728ea71e02dff228c7
Lucas Magalhaes <lucasdecassia9@hotmail.com>
2024-08-22 11:29:54 -0300

    portfolio

--- src/lib/Menu.svelte
+++ src/lib/Menu.svelte
@@ -1,19 +1,33 @@
 <script>
 
     import Logo from "./Logo.svelte";
+    import { page } from "$app/stores";
 
+    const items = [
+        {href:"/", label: 'Início'},
+        {href:"sobre", label: 'Sobre'},
+        {href:"/portfolio", label: 'Portfolio'},
+        {href:"/contato", label: 'Contato'},
+        {href:"/faq", label: 'Perguntas Frequentes'},
+
+    ]
+
+    const p = $page.url
+    console.log(p)
 </script>
 
 <a href="/">
     <Logo/>
    </a>
 
-<a href="/" class="text-gray-800 transition-colors duration-300 transform dark:text-gray-200 border-b-2 border-blue-500 mx-1.5 sm:mx-6">home</a>
+{#each items as {href, label} }
+{#if $page.url.pathname === href}
+<a {href} class="text-gray-800 transition-colors duration-300 transform dark:text-gray-200 border-b-2 border-blue-500 mx-1.5 sm:mx-6">{label}</a> 
+    {:else}
+<a {href} class="border-b-2 border-transparent hover:text-gray-800 transition-colors duration-300 transform dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6">{label}</a>
+{/if}
+{/each}
 
-<a href="/sobre" class="border-b-2 border-transparent hover:text-gray-800 transition-colors duration-300 transform dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6">Sobre</a>
 
-<a href="/portfolio" class="border-b-2 border-transparent hover:text-gray-800 transition-colors duration-300 transform dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6">Portfolio</a>
 
-<a href="/contato" class="border-b-2 border-transparent hover:text-gray-800 transition-colors duration-300 transform dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6">Contato</a>
 
-<a href="/faq" class="border-b-2 border-transparent hover:text-gray-800 transition-colors duration-300 transform dark:hover:text-gray-200 hover:border-blue-500 mx-1.5 sm:mx-6">Perguntas frequentes</a>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,11 +4,7 @@
 </script>
 <h1>Inínio</h1>
 
-{#if idade > 20 && idade <30}
-<p>Idade está entre 20 e 30</p>
-{:else}
-<p>Maior que 30</p>
-{/if}
+
 
 
 
