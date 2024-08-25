12b4ef49e6fa89178c5e17e7367c5ab2cad2db96
Lucas Magalhaes <lucasdecassia9@hotmail.com>
2024-08-25 13:30:23 -0300

    details portfolio

---
+++ src/routes/portfolio/+page.ts
@@ -0,0 +1,17 @@
+export function load(){
+    return{
+
+        categories: [
+            'Todos',
+            'E commerce',
+            'Dentista',
+            'Produtos'
+        ],
+
+        items : [
+            {tittle: 'Loja Virtual', category: 'E commerce', image:'/paginaVenda.png' },
+            {tittle: 'Perfil Profissional', category: 'Dentista', image:'dente.png' },
+            {tittle: 'Lista de Produtos', category: 'Produtos', image:'produto.png' }
+            ]
+    }
+}
