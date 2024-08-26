d92f65567ac4aeb81dbdd149b26bba0f3cf8e58e
Lucas Magalhaes <lucasdecassia9@hotmail.com>
2024-08-26 17:50:03 -0300

    teste access github

--- tests/teste.spec.ts
+++ tests/teste.spec.ts
@@ -19,3 +19,33 @@ test('Clique no link "Ver Produtos" e verifique a navegação', async ({ page })
 });
 
 
+test('Desça até o final da página e clique no link do GitHub', async ({ page }) => {
+  // Acesse a página inicial
+  await page.goto('http://localhost:5173/');
+
+  // Desça até o final da página
+  await page.evaluate(() => {
+    window.scrollTo(0, document.body.scrollHeight);
+  });
+
+  // Espera até que o link do GitHub esteja visível
+  const githubLink = page.locator('a[aria-label="Github"]');
+  await expect(githubLink).toBeVisible();
+
+  // Clica no link do GitHub
+  await githubLink.click();
+
+  // Aguarda o novo navegador ou aba abrir e verificar a URL
+  const [newPage] = await Promise.all([
+    page.context().waitForEvent('page'), // Espera por uma nova página ser aberta
+    githubLink.click(), // Clica no link do GitHub
+  ]);
+
+  // Espera até que a nova página carregue
+  await newPage.waitForLoadState();
+
+  // Verifica se a nova página é a URL correta do GitHub
+  await expect(newPage).toHaveURL('https://github.com/imalhaas');
+});
+
+
